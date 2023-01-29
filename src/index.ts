57f08ccf20ca54e2177f086a634fae52381e4b50
muratj <muratj.tm@gmail.com>
2023-01-29 00:38:03 -0500

    express base setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world');
+import express from 'express';
+
+// Constants
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// App Configurations
+app.use(express.json());
+
+app.get('/hello', (req, res) => {
+	res.send({ greeting: 'Hello world' });
+});
+
+// Bootstrap
+app.listen(PORT, () => {
+	console.log(`App is running on port ${PORT}`);
+});
